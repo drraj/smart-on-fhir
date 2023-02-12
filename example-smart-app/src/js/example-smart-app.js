16313159deff6334da4e80ca9651973cbf8b154a
drraj <doc_raja@yahoo.com>
2023-02-11 20:07:53 -0800

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -137,7 +137,7 @@
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
     $('#height').html(p.height);
-    $('#weight').html(p.weight.toFixed(2));
+    $('#weight').html(p.weight);
     $('#bmi').html(p.bmi);
     
      $('#systolicbp').html(p.systolicbp);
